28591decfd73a2398a4674e3a6ad2607c52ce1fa
shadan-behzadian <shadan.behzadian@gmail.com>
2021-03-10 16:40:34 +0100

    First-commit meditation app completed

---
+++ app.js
@@ -0,0 +1,21 @@
+const container = document.querySelector(".container");
+const text = document.getElementById("text");
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breatheAnimation();
+
+function breatheAnimation() {
+  text.innerHTML = "Breathe in!";
+  container.className = "container grow";
+  setTimeout(() => {
+    text.innerHTML = "Hold";
+    setTimeout(() => {
+      text.innerHTML = "Breathe Out";
+      container.className = "container shrink";
+    }, holdTime);
+  }, breatheTime);
+}
+
+setInterval(breatheAnimation, totalTime);
